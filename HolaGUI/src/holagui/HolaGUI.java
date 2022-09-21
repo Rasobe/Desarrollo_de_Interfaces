fa8a7760b3d5090974f3b49c23af034a4a80ed4d
DAM2Alu13 <DAM2Alu13@B01-PC05.IESGUARA.LOCAL>
2022-09-21 13:04:04 +0200

    Inicio de NetBeans con Git.

--- HolaGUI/src/holagui/HolaGUI.java
+++ HolaGUI/src/holagui/HolaGUI.java
@@ -14,7 +14,7 @@ public class HolaGUI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Holaa");
     }
     
 }
