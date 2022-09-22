1e6da7c393776fce065e982254ab7ba7979941ef
DAM2Alu13 <DAM2Alu13@B01-PC05.IESGUARA.LOCAL>
2022-09-22 12:45:26 +0200

    Prueba

---
+++ Desarrollo_de_Interfaces/src/desarrollo_de_interfaces/Desarrollo_de_Interfaces.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package desarrollo_de_interfaces;
+
+/**
+ *
+ * @author DAM2Alu13
+ */
+public class Desarrollo_de_Interfaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
